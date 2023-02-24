98c87e81fcad0f704695c94b722ca4e0c013d994
Ankush Waghmare <waghmareankush050@gmail.com>
2023-02-24 12:53:55 +0530

    first commit

--- script.js
+++ script.js
@@ -15,17 +15,46 @@
 //console.log(document.links)
 //console.log(document.images)
 
-console.log(document.getElementById("header-title"));
+//console.log(document.getElementById("header-title"));
 
-let headertitle = document.getElementById("header-title");
-let mainheader = document.getElementById("main-header");
-console.log(headertitle)
+//let headertitle = document.getElementById("header-title");
+//let mainheader = document.getElementById("main-header");
+//console.log(headertitle)
 
-headertitle.textContent = "Bye"
-headertitle.innerText = "Joker"
+//headertitle.textContent = "Bye"
+//headertitle.innerText = "Joker"
 
-headertitle.innerHTML = "<h1>Hello</h1>"
+//headertitle.innerHTML = "<h1>Hello</h1>"
 
-headertitle.style.borderBottom = "solid 3px black"
-mainheader.style.border = "solid 3px black"
+//headertitle.style.borderBottom = "solid 3px black"
+//mainheader.style.border = "solid 3px black"
+
+
+
+
+
+// GET ELEMENT BY CLASS NAME
+
+let items = document.getElementsByClassName("list-group-item")
+console.log(items)
+console.log(items[0])
+items[0].textContent = "Ankush"
+items[0].style.fontWeight = "bold"
+items[1].style.fontWeight = "bold"
+items[2].style.fontWeight = "bold"
+items[3].style.fontWeight = "bold"
+items[2].style.backgroundColor = "green"
+
+//color code also work for separate elements
+//items[2].style.backgroundColor = "#f4f4f4"
+
+
+// THis will not work
+// items.style.backgroundColor = "#f4f4f4"
+
+
+//This will work
+//for(i=0;i<items.length;i++){
+    //items[i].style.backgroundColor = "#f4f4f4"
+//}
 
